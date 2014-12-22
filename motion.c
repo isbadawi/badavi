d5e163785014f2cb6b8ca49c7b821515df5d0e42
Ismail Badawi <ismail@badawi.io>
2014-12-22 17:46:47 -0500

    Add 'w' motion.
    Works wih 'd' and 'c', although vim has a special case where although w
    move to the next line if needed, 'dw' and 'cw' will only delete till the
    end of the line. Will figure this out with the other linewise stuff.

--- motion.c
+++ motion.c
@@ -58,6 +58,52 @@ static pos_t buffer_bottom(pos_t pos, window_t *window) {
   return pos;
 }
 
+static char char_at(pos_t pos) {
+  return pos.line->buf->buf[pos.offset];
+}
+
+static pos_t next_char(pos_t pos) {
+  int len = pos.line->buf->len;
+  if (pos.offset < len - 1 || (pos.offset == len - 1 && !pos.line->next)) {
+    pos.offset++;
+  } else if (pos.line->next) {
+    pos.line = pos.line->next;
+    pos.offset = 0;
+  }
+  return pos;
+}
+
+static int is_word_char(char c) {
+  return isalnum(c) || c == '_';
+}
+
+static int is_word_start(pos_t pos) {
+  char c = char_at(pos);
+  if (isspace(c)) {
+    return 0;
+  }
+  if (pos.offset == 0) {
+    return 1;
+  }
+  pos.offset--;
+  char last = char_at(pos);
+  if (isspace(last)) {
+    return 1;
+  }
+  return is_word_char(c) + is_word_char(last) == 1;
+}
+
+static int last_char(pos_t pos) {
+  return pos.offset == pos.line->buf->len && !pos.line->next;
+}
+
+static pos_t next_word_start(pos_t pos, window_t *window) {
+  do {
+    pos = next_char(pos);
+  } while (!is_word_start(pos) && !last_char(pos));
+  return pos;
+}
+
 int motion_key(motion_t *motion, struct tb_event *ev) {
   if (!isdigit(motion->last)) {
     motion->count = 0;
@@ -81,6 +127,7 @@ int motion_key(motion_t *motion, struct tb_event *ev) {
   case 'j': motion->op = down; break;
   case 'k': motion->op = up; break;
   case 'l': motion->op = right; break;
+  case 'w': motion->op = next_word_start; break;
   // TODO(isbadawi): G should jump to line "count", but ops can't access that.
   case 'G': motion->op = buffer_bottom; break;
   case 'g':
